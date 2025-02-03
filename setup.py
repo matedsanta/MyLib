f02551886f99b7d54e39bb21c367ba4d1921bd3a
matedsanta <santa.mate.daniel@gmail.com>
2025-02-03 11:58:47 +0100

    added setup.py for PIP uspport

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smlib",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Mate Daniel Santa",
+    description="A Python module for myself",
+    url="https://github.com/matedsanta/MyLib",
+)
